53fc2e35ce5df5cbfbffafd30888db2fa7c4a7f4
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2021-08-17 16:06:21 -0400

    Commit the probably busted collector

--- src/service/utils/EventCollector.ts
+++ src/service/utils/EventCollector.ts
@@ -3,26 +3,28 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { Disposable, Emitter, ServerRequestHandler } from 'vscode-languageserver'
+import { CancellationToken, Disposable, HandlerResult, ResultProgressReporter, ServerRequestHandler, WorkDoneProgressReporter } from 'vscode-languageserver'
 
 /**
  * Allows multiplexing one LSP event to multiple event handlers.
  * Necessary due to https://github.com/microsoft/vscode-languageserver-node/issues/174
  */
-export class EventCollector<P, R, PR, E> implements ServerRequestHandler<P, R, PR, E>, Disposable {
-    private readonly emitter = new Emitter<ServerRequestHandler<P, R, PR, E>>(
-        {
-            onLastListenerRemove: () => this.dispose()
-        }
-    );
+export class CollectorEmitter<P, R extends Array<unknown>, PR, E> implements Disposable {
+    private readonly subscribers: ServerRequestHandler<P, R, PR, E>[] = [];
 
     public dispose(): void {
         this.emitter.dispose();
     }
 
-    public onEvent = this.emitter.event;
+    public async fire(params: P, token: CancellationToken, workDoneProgress: WorkDoneProgressReporter, resultProgress?: ResultProgressReporter<PR>): HandlerResult<R, E> {
+        return await Promise.all(
+            this.subscribers.map(s => s(p, token, workDoneProgress, resultProgress))
+        )
+    }
 
-    public fire(params: P): R {
-        this.emitter.fire(params);
+    public subscribe(handler: ServerRequestHandler<P, R, PR, E>): Disposable {
+        this.subscribers.push(handler);
+
+        return undefined; // TODO
     }
 }
