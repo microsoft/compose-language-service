ff0e2870178af9a1687ce55fdd109e3dfab3ee82
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2021-08-23 10:42:40 -0400

    Static-ify ExtendedPosition

--- src/service/ExtendedPosition.ts
+++ src/service/ExtendedPosition.ts
@@ -4,39 +4,37 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { Position } from 'vscode-languageserver-textdocument';
-import { CachedDocument } from './CachedDocument';
 import { CST } from 'yaml';
+import { CachedDocument } from './CachedDocument';
 
 type ItemType = 'start' | 'key' | 'sep' | 'value';
 
 export class ExtendedPosition {
-    private readonly path: CST.VisitPath;
-    private readonly offset: number;
+    private constructor(
+        public readonly item: CST.CollectionItem,
+        public readonly itemType: ItemType,
+        public readonly logicalPath: string,
+    ) { }
 
-    public readonly item: CST.CollectionItem;
-    public readonly type: ItemType;
-    public readonly logicalPath: string;
+    public static build(cachedDocument: CachedDocument, position: Position): ExtendedPosition {
+        const offset = cachedDocument.textDocument.offsetAt(position);
 
-    public constructor(private readonly cachedDocument: CachedDocument, private readonly position: Position) {
-        this.offset = this.cachedDocument.textDocument.offsetAt(this.position);
-
-        const { item, path } = this.loadItemAndPath();
+        const { item, path } = ExtendedPosition.loadItemAndPath(cachedDocument.cst, offset);
         if (!item || !path) {
             throw new Error(`Failed to load item at position (${position.line}, ${position.character}).`);
         }
 
-        this.item = item;
-        this.path = path;
+        const itemType = ExtendedPosition.loadType(item, offset);
+        const logicalPath = ExtendedPosition.loadLogicalPath(cachedDocument.cst, item, path, itemType);
 
-        this.type = this.loadType();
-        this.logicalPath = this.loadLogicalPath();
+        return new ExtendedPosition(item, itemType, logicalPath);
     }
 
-    private loadItemAndPath(): { item: CST.CollectionItem | undefined, path: CST.VisitPath | undefined } {
+    private static loadItemAndPath(cst: CST.Document, offset: number): { item: CST.CollectionItem | undefined, path: CST.VisitPath | undefined } {
         let currentItem: CST.CollectionItem | undefined;
         let currentPath: CST.VisitPath | undefined;
 
-        CST.visit(this.cachedDocument.cst, (item, path) => {
+        CST.visit(cst, (item, path) => {
             const itemStart =
                 item.start?.[0]?.offset ??
                 item.key?.offset ??
@@ -44,7 +42,7 @@ export class ExtendedPosition {
                 item.value?.offset ??
                 -1;
 
-            if (itemStart > this.offset) {
+            if (itemStart > offset) {
                 // As soon as we get past the offset, break;
                 // The previous item is the `CollectionItem` at the position specified
                 return CST.visit.BREAK;
@@ -61,23 +59,23 @@ export class ExtendedPosition {
         };
     }
 
-    private loadType(): ItemType {
-        for (const start of this.item.start) {
-            if (start.offset <= this.offset &&
-                start.offset + start.source.length > this.offset) {
+    private static loadType(item: CST.CollectionItem, offset: number): ItemType {
+        for (const start of item.start) {
+            if (start.offset <= offset &&
+                start.offset + start.source.length > offset) {
                 return 'start';
             }
         }
 
-        if (CST.isScalar(this.item.key) &&
-            this.item.key.offset <= this.offset &&
-            this.item.key.offset + this.item.key.source.length > this.offset) {
+        if (CST.isScalar(item.key) &&
+            item.key.offset <= offset &&
+            item.key.offset + item.key.source.length > offset) {
             return 'key';
         }
 
-        for (const sep of this.item.sep ?? []) {
-            if (sep.offset <= this.offset &&
-                sep.offset + sep.source.length > this.offset) {
+        for (const sep of item.sep ?? []) {
+            if (sep.offset <= offset &&
+                sep.offset + sep.source.length > offset) {
                 return 'sep';
             }
         }
@@ -86,22 +84,22 @@ export class ExtendedPosition {
         return 'value';
     }
 
-    private loadLogicalPath(): string {
+    private static loadLogicalPath(cst: CST.Document, item: CST.CollectionItem, path: CST.VisitPath, itemType: ItemType): string {
         const resultParts: string[] = [];
 
         // If it's a key, we want the actual key text as the logical path
         // Otherwise we just put <theType>
-        if (this.type === 'key' && CST.isScalar(this.item.key)) {
-            resultParts.unshift(this.item.key.source);
+        if (itemType === 'key' && CST.isScalar(item.key)) {
+            resultParts.unshift(item.key.source);
         } else {
-            resultParts.unshift(`<${this.type}>`);
+            resultParts.unshift(`<${itemType}>`);
         }
 
         // To get the rest of the logical path, we must step backward to the root
         // In properly formatted compose documents it will be keys the rest of the way up
-        for (let i = this.path.length - 1; i > 0; i--) {
-            const parentPath = this.path.slice(0, i);
-            const parent = CST.visit.itemAtPath(this.cachedDocument.cst, parentPath);
+        for (let i = path.length - 1; i > 0; i--) {
+            const parentPath = path.slice(0, i);
+            const parent = CST.visit.itemAtPath(cst, parentPath);
             if (CST.isScalar(parent?.key)) {
                 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
                 resultParts.unshift(parent!.key.source);
--- src/service/providers/KeyHoverProvider.ts
+++ src/service/providers/KeyHoverProvider.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { CancellationToken, Hover, HoverParams } from 'vscode-languageserver';
+import { CancellationToken, Hover, HoverParams, MarkupContent } from 'vscode-languageserver';
 import { CST } from 'yaml';
 import { CachedDocument } from '../CachedDocument';
 import { ExtendedPosition } from '../ExtendedPosition';
@@ -11,21 +11,34 @@ import { yamlRangeToLspRange } from '../utils/yamlRangeToLspRange';
 
 export class KeyHoverProvider {
     public static async onHover(params: HoverParams & { cachedDocument: CachedDocument }, token: CancellationToken): Promise<Hover | undefined> {
-        const extendedPosition = new ExtendedPosition(params.cachedDocument, params.position);
+        const extendedPosition = ExtendedPosition.build(params.cachedDocument, params.position);
 
-        if (extendedPosition.type === 'key' && CST.isScalar(extendedPosition.item.key)) {
-            const key = extendedPosition.item.key;
-            const keyName = key.source;
+        if (extendedPosition.itemType === 'key' && CST.isScalar(extendedPosition.item.key)) {
+            const keyInfo = ComposeKeyInfo.find((k) => k.pathRegex.test(extendedPosition.logicalPath));
 
-            return {
-                contents: {
-                    kind: 'markdown',
-                    value: 'TODO'
-                },
-                range: yamlRangeToLspRange(params.cachedDocument.textDocument, [key.offset, key.offset + keyName.length]),
-            };
+            if (keyInfo) {
+                return {
+                    contents: keyInfo.contents,
+                    range: yamlRangeToLspRange(params.cachedDocument.textDocument, [extendedPosition.item.key.offset, extendedPosition.item.key.offset + extendedPosition.item.key.source.length]),
+                };
+            }
         }
 
         return undefined;
     }
 }
+
+interface ComposeKeyInformation {
+    pathRegex: RegExp,
+    contents: MarkupContent,
+}
+
+const ComposeKeyInfo: ComposeKeyInformation[] = [
+    {
+        pathRegex: /^\/services$/i,
+        contents: {
+            kind: 'markdown',
+            value: 'The services in your compose project',
+        },
+    },
+];
