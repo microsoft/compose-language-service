a0f863cf2d076e290490b53c9128dfa511b83976
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2021-08-25 14:20:47 -0400

    Add ports provider

--- src/service/providers/signatureHelp/PortsSignatureHelpProvider.ts
+++ src/service/providers/signatureHelp/PortsSignatureHelpProvider.ts
@@ -3,12 +3,29 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { CancellationToken, SignatureHelp, SignatureHelpParams } from 'vscode-languageserver';
+import { CancellationToken, ParameterInformation, SignatureHelp, SignatureHelpParams, SignatureInformation } from 'vscode-languageserver';
 import { ExtendedPositionParams } from '../../ExtendedParams';
 import { SubproviderBase } from '../MultiProviderBase';
 
 export class PortsSignatureHelpProvider implements SubproviderBase<SignatureHelpParams & ExtendedPositionParams, SignatureHelp, never> {
     public on(params: SignatureHelpParams & ExtendedPositionParams, token: CancellationToken): SignatureHelp | undefined {
-        return undefined;
+        if (!/\/services\/\w+\/ports\/<value>/i.test(params.extendedPosition.logicalPath)) {
+            return undefined;
+        }
+
+        const result: SignatureHelp = {
+            signatures: [],
+            activeSignature: null,
+            activeParameter: null,
+        };
+
+        result.signatures.push(
+            SignatureInformation.create('Short syntax', undefined,
+                ParameterInformation.create('- 5000:5000'),
+                ParameterInformation.create('- 5000/tcp:5000/tcp')
+            )
+        );
+
+        return result;
     }
 }
