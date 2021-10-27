211dcd3a5b09679d01232ef3367bf48cfd0b49a8
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2021-10-27 12:31:57 -0400

    Commentary on logNormal

--- src/service/utils/telemetry/logNormal.ts
+++ src/service/utils/telemetry/logNormal.ts
@@ -3,6 +3,12 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+/**
+ * Fits a lognormal distribution to the given data and returns the mu, sigma, and median of the data
+ * See also: https://en.wikipedia.org/wiki/Log-normal_distribution
+ * @param values The list of values to fit a lognormal distribution to
+ * @returns The mu (log of median), sigma (stdev), and median, fit with a lognormal distribution
+ */
 export function logNormal(values: number[]): { mu: number, sigma: number, median: number } {
     if (!values?.length) {
         return { mu: 0, sigma: 0, median: 0 };
@@ -12,8 +18,11 @@ export function logNormal(values: number[]): { mu: number, sigma: number, median
     const lnValues = values.map(a => ln(a));
     const sqLnValues = lnValues.map(a => sq(a));
 
+    // Mu is the mean of the natural logs of the values
     const mu = sum(lnValues) / n;
 
+    // Sigma is calculated from the natural logs and their squares
+    // If there's only 1 element sigma must be 0
     let sigma: number;
     if (n > 1) {
         sigma = Math.sqrt(
@@ -31,14 +40,23 @@ export function logNormal(values: number[]): { mu: number, sigma: number, median
     };
 }
 
+/**
+ * Adds all elements of an array
+ */
 function sum(values: number[]): number {
     return values.reduce((a, b) => a + b, 0);
 }
 
+/**
+ * Squares the number. This function is added to make the above sigma calculation more readable.
+ */
 function sq(value: number): number {
     return Math.pow(value, 2);
 }
 
+/**
+ * Gets the natural log of a number. This function is added to make the above sigma calculation more readable.
+ */
 function ln(value: number): number {
     return Math.log(value);
 }
