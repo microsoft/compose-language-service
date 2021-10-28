5544e71824ed61a2d786c8f38775de38fa6f4766
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2021-10-28 11:28:47 -0400

    Bug fixes

--- src/service/ComposeLanguageService.ts
+++ src/service/ComposeLanguageService.ts
@@ -48,7 +48,7 @@ export class ComposeLanguageService implements Disposable {
         this.createLspHandler(this.connection.onDocumentFormatting, new DocumentFormattingProvider());
 
         // Hook up one additional notification handler
-        this.connection.onNotification(DocumentSettingsNotification.type, this.onDidChangeDocumentSettings);
+        this.connection.onNotification(DocumentSettingsNotification.method, (params) => this.onDidChangeDocumentSettings(params));
 
         // Start the document listener
         this.documentManager.listen(this.connection);
