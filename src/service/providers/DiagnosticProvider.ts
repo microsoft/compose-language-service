f3d721cd7a0d4e4fec26cf1810bb839d83637a38
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2021-11-01 13:45:07 -0400

    Start to add schema validation capabilities

--- src/service/providers/DiagnosticProvider.ts
+++ src/service/providers/DiagnosticProvider.ts
@@ -3,7 +3,9 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { Diagnostic, DiagnosticSeverity, TextDocumentChangeEvent } from 'vscode-languageserver';
+import { default as Ajv, ValidateFunction } from 'ajv';
+import * as fs from 'fs';
+import { Diagnostic, DiagnosticSeverity, Range, TextDocumentChangeEvent } from 'vscode-languageserver';
 import { ComposeDocument } from '../ComposeDocument';
 import { ExtendedParams } from '../ExtendedParams';
 import { getCurrentContext } from '../utils/ActionContext';
@@ -16,6 +18,15 @@ import { ProviderBase } from './ProviderBase';
 const DiagnosticDelay = 1000;
 
 export class DiagnosticProvider extends ProviderBase<TextDocumentChangeEvent<ComposeDocument> & ExtendedParams, void, never, never> {
+    private readonly validate: ValidateFunction;
+    public constructor() {
+        super();
+
+        const schema = JSON.parse(fs.readFileSync('./resources/compose-spec.json', 'utf-8'));
+        const ajv = new Ajv({ allErrors: true });
+        this.validate = ajv.compile(schema);
+    }
+
     public on(params: TextDocumentChangeEvent<ComposeDocument> & ExtendedParams): void {
         const ctx = getCurrentContext();
         if (!ctx.clientCapabilities.textDocument?.publishDiagnostics) {
@@ -25,23 +36,50 @@ export class DiagnosticProvider extends ProviderBase<TextDocumentChangeEvent<Com
         ctx.telemetry.suppressAll = true; // Diagnostics is async and telemetry won't really work
 
         debounce(DiagnosticDelay, { uri: params.document.textDocument.uri, callId: 'diagnostics' }, () => {
-            const diagnostics: Diagnostic[] = [];
-
-            for (const error of [...params.document.yamlDocument.value.errors, ...params.document.yamlDocument.value.warnings]) {
-                diagnostics.push(
-                    Diagnostic.create(
-                        yamlRangeToLspRange(params.document.textDocument, error.pos),
-                        error.message,
-                        error.name === 'YAMLWarning' ? DiagnosticSeverity.Warning : DiagnosticSeverity.Error,
-                        error.code,
-                    )
-                );
-            }
+            const diagnostics: Diagnostic[] = this.getDiagnostics(params);
 
             ctx.connection.sendDiagnostics({
                 uri: params.document.textDocument.uri,
                 diagnostics: diagnostics,
             });
         }, this);
+    }
+
+    private getDiagnostics(params: ExtendedParams): Diagnostic[] {
+        const results: Diagnostic[] = [];
+
+        const syntaxErrors = [...params.document.yamlDocument.value.errors, ...params.document.yamlDocument.value.warnings];
+
+        for (const error of syntaxErrors) {
+            results.push(
+                Diagnostic.create(
+                    yamlRangeToLspRange(params.document.textDocument, error.pos),
+                    error.message,
+                    error.name === 'YAMLWarning' ? DiagnosticSeverity.Warning : DiagnosticSeverity.Error,
+                    error.code,
+                )
+            );
+        }
+
+        if (results.length > 0) {
+            // If there are syntax errors, we will not go on to check for schema errors
+            return results;
+        }
+
+        if (!this.validate(params.document.yamlDocument.value.toJS())) {
+            const schemaErrors = this.validate.errors ?? [];
+
+            for (const error of schemaErrors) {
+                results.push(
+                    Diagnostic.create(
+                        Range.create(0, 0, 0, 0), // TODO: compute the range? Is that even possible?
+                        error.message ?? 'foo',
+                        DiagnosticSeverity.Error,
+                    )
+                );
+            }
+        }
+
+        return results;
     }
 }
