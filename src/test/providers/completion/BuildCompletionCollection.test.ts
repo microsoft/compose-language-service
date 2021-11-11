79dced84426c8a1846aa84fdf75aa03222e5687e
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2021-11-11 13:37:01 -0500

    Add test file stub

---
+++ src/test/providers/completion/BuildCompletionCollection.test.ts
@@ -0,0 +1,16 @@
+/*!--------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+// TODO
+describe('BuildCompletionCollection', () => {
+    describe('Common scenarios', () => {
+        xit('Should have tests');
+    });
+
+    describe('Error scenarios', () => {
+        xit('Should return an error for nonexistent files');
+        xit('Should have tests');
+    });
+});
