080fe627d9edb93737cd7930f6c9d1a4809adec4
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-10-02 13:46:44 -0400

    Upgrade to NodeJS 22

--- src/test/global.test.ts
+++ src/test/global.test.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as chai from 'chai';
-import * as chaiAsPromised from 'chai-as-promised';
+import chaiAsPromised from 'chai-as-promised';
 
 before('Global setup', function () {
     console.log('Global setup');
