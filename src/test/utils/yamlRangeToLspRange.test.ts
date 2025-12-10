ac518eedecc23878eeb094a6a65c0a0e9c28c99b
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-12-10 12:27:52 -0500

    Fix

--- src/test/utils/yamlRangeToLspRange.test.ts
+++ src/test/utils/yamlRangeToLspRange.test.ts
@@ -5,30 +5,30 @@
 
 import { TextDocument } from 'vscode-languageserver-textdocument';
 import { Range } from 'vscode-languageserver-types';
-import { Range as YamlRange } from 'yaml/dist/nodes/Node';
+import type { Range as YamlRange } from 'yaml';
 import { yamlRangeToLspRange } from '../../service/utils/yamlRangeToLspRange';
 
 describe('(Unit) yamlRangeToLspRange', () => {
-    describe('Common scenarios', () => {
-        it('Should return the correct result for two-integer ranges', () => {
-            const doc = TextDocument.create('file:///foo', 'dockercompose', 1, `version: '123'
+  describe('Common scenarios', () => {
+    it('Should return the correct result for two-integer ranges', () => {
+      const doc = TextDocument.create('file:///foo', 'dockercompose', 1, `version: '123'
 services:
   foo:
     image: redis`);
 
-            const result = yamlRangeToLspRange(doc, [4, 29]);
-            result.should.deep.equal(Range.create(0, 4, 2, 4));
-        });
-
-        it('Should return the correct result for yaml `Range` objects', () => {
-            const doc = TextDocument.create('file:///foo', 'dockercompose', 1, `version: '123'
-services:
-  foo:
-    image: redis`);
-
-            const yamlRange: YamlRange = [4, 29, 30];
-            const result = yamlRangeToLspRange(doc, yamlRange);
-            result.should.deep.equal(Range.create(0, 4, 2, 4));
-        });
+      const result = yamlRangeToLspRange(doc, [4, 29]);
+      result.should.deep.equal(Range.create(0, 4, 2, 4));
     });
+
+    it('Should return the correct result for yaml `Range` objects', () => {
+      const doc = TextDocument.create('file:///foo', 'dockercompose', 1, `version: '123'
+services:
+  foo:
+    image: redis`);
+
+      const yamlRange: YamlRange = [4, 29, 30];
+      const result = yamlRangeToLspRange(doc, yamlRange);
+      result.should.deep.equal(Range.create(0, 4, 2, 4));
+    });
+  });
 });
