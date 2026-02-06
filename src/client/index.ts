f8840380637457dbe6e6113a0b624baa6a7cf425
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2026-02-06 14:00:03 -0500

    Export one more thing

--- src/client/index.ts
+++ src/client/index.ts
@@ -3,7 +3,8 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-export * from '../common/DocumentSettingsClientCapabilities';
-export * from '../common/TelemetryEvent';
 export * from '../common/AlternateYamlLanguageServiceClientCapabilities';
 export * from '../common/ComposeLanguageClientCapabilities';
+export * from '../common/DocumentSettingsClientCapabilities';
+export * from '../common/TelemetryEvent';
+export * from '../common/TextDocumentParams';
